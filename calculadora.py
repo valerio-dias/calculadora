07f92b65f81afa23ef30763d41eea7df4b470c0f
Valerio Dias <valeriods@gmail.com>
2021-11-30 10:58:22 -0300

    criação da classe de operacoes

--- calculadora.py
+++ calculadora.py
@@ -3,7 +3,10 @@
 class Calculadora:
     def __init__(self):
         self.resultado = 0
-        self.operando = 0       
+
+    def operando(self, numero):
+        self.resultado = numero
+        return self.resultado
 
     def somar(self, numero):
         self.resultado += numero
@@ -27,7 +30,4 @@ class Calculadora:
 
     def limpar(self):
         self.resultado = 0
-        self.operando = 0
         return self.resultado
-
-    
--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-from tkinter import *
+# from tkinter import *
+from calculadora import Calculadora
 
-menu_inicial = Tk()
-menu_inicial.title("Calculadora")
-menu_inicial.mainloop()
+# # menu_inicial = Tk()
+# # menu_inicial.title("Calculadora")
+# # menu_inicial.mainloop()
+calc = Calculadora()
+
+calc.operando(20)
+print(calc.somar(10)) # teste de classe
+
+calc.limpar()
+calc.operando(60)
+print(calc.subtrair(20)) # teste de classe
+
+calc.limpar()
+calc.operando(6)
+print(calc.multiplicar(20)) # teste de classe
+
+calc.limpar()
+calc.operando(60)
+print(calc.dividir(20)) # teste de classe
+
+calc.limpar()
+calc.operando(3)
+print(calc.potencia(3)) # teste de classe
