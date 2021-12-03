0d5bf810934a67cd27a520bd4135938a50ab3a44
Valerio Dias <valeriods@gmail.com>
2021-12-03 12:19:35 -0300

    projeto funcionando

--- main.py
+++ main.py
@@ -4,54 +4,102 @@ from calculadora import Calculadora
 tk = Tk()
 tk.title("Calculadora")
 tk['bg'] = '#BBBBBB'
-tk.geometry("220x324+0+0")
+tk.geometry("220x400+0+0")
 tk.resizable(False, False)
 tk.iconbitmap("images/icon.ico")
 
-bt1 = Button(tk, text="7", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=10, y=100)
+def btn_click(item):
+    global valor
 
-bt1 = Button(tk, text="8", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=60, y=100)
+    valor = valor + str(item)
 
-bt1 = Button(tk, text="9", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=110, y=100)
+    input_valor.set(valor)
 
-bt1 = Button(tk, text="*", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=160, y=100)
 
-bt1 = Button(tk, text="4", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=10, y=160)
+def btn_clear():
+    global valor
 
-bt1 = Button(tk, text="5", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=60, y=160)
+    valor = ""
 
-bt1 = Button(tk, text="6", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=110, y=160)
+    input_valor.set("")
 
-bt1 = Button(tk, text="+", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=160, y=160)
 
-bt1 = Button(tk, text="1", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=10, y=210)
+def btn_igual():
+    global valor
 
-bt1 = Button(tk, text="2", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=60, y=210)
+    resultado = str(eval(valor)) 
 
-bt1 = Button(tk, text="3", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=110, y=210)
+    input_valor.set(resultado)
 
-bt1 = Button(tk, text="-", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=160, y=210)
 
-bt1 = Button(tk, text="-", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=160, y=160)
+valor = " "
 
-bt1 = Button(tk, text="-", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=160, y=160)
+input_valor = StringVar()
 
-bt1 = Button(tk, text="-", bg="#000000",fg = "white", font=("Arial", 12, "bold"), padx=10, pady=10, bd=2)
-bt1.place(x=160, y=160)
+input_frame = Frame(tk, width=210, height=85, bd=2, bg="lightgray", highlightbackground="black", highlightcolor="black",
+                    highlightthickness=1, padx=10, pady=10)
+
+input_frame.pack(side=TOP)
+
+input_field = Entry(input_frame, font=('arial', 18, 'bold'), textvariable=input_valor, width=50, bg="#EEE", bd=0,
+                    justify=RIGHT)
+
+input_field.grid(row=0, column=0)
+
+input_field.pack(ipady=10)  
+
+
+
+clear = Button(tk, text="C", bg="#882222",fg = "white", width=11, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_clear())
+clear.place(x=10, y=100)
+
+divisao = Button(tk, text="/", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click("/"))
+divisao.place(x=160, y=100)
+
+sete = Button(tk, text="7", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click(7))
+sete.place(x=10, y=160)
+
+oito = Button(tk, text="8", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click(8))
+oito.place(x=60, y=160)
+
+nove = Button(tk, text="9", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click(9))
+nove.place(x=110, y=160)
+
+multiplicacao = Button(tk, text="*", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click("*"))
+multiplicacao.place(x=160, y=160)
+
+quatro = Button(tk, text="4", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click(4))
+quatro.place(x=10, y=220)
+
+cinco = Button(tk, text="5", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click(5))
+cinco.place(x=60, y=220)
+
+seis = Button(tk, text="6", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click(6))
+seis.place(x=110, y=220)
+
+soma = Button(tk, text="+", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click("+"))
+soma.place(x=160, y=220)
+
+um = Button(tk, text="1", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click(1))
+um.place(x=10, y=280)
+
+dois = Button(tk, text="2", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click(2))
+dois.place(x=60, y=280)
+
+tres = Button(tk, text="3", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click(3))
+tres.place(x=110, y=280)
+
+subtracao = Button(tk, text="-", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click("-"))
+subtracao.place(x=160, y=280)
+
+zero = Button(tk, text="0", bg="#000000",fg = "white", width=6, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click(0))
+zero.place(x=10, y=340)
+
+ponto = Button(tk, text=".", bg="#000000",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_click("."))
+ponto.place(x=110, y=340)
+
+igual = Button(tk, text="=", bg="#005511",fg = "white", width=1, height= 1, font=("Arial", 12, "bold"), padx=10, pady=10, bd=2, cursor="hand2",command=lambda: btn_igual())
+igual.place(x=160, y=340)
 
 
 tk.mainloop()
